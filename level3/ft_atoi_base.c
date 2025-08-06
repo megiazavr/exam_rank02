85cba48e9bba8bcc29b5cd8bf62d7841cde1319d
Milena Egiazarian <megiazar@c2r8s13.42lisboa.com>
2025-08-06 20:26:48 +0100

    atoi_base

---
+++ level3/ft_atoi_base.c
@@ -0,0 +1,13 @@
+int ft_atoi_base(const char *str, int str_base);
+int isvalid(int ch, int baselen);
+
+
+int ft_atoi_base(const char *str, int str_base)
+{
+
+}
+
+int main(int ac, char *av[])
+{
+
+}
